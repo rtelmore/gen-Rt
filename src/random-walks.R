a8bcf0442e40f223dc8d419ba05ed4c623deb77d
Ryan Elmore <Ryan.Elmore@du.edu>
2019-10-29 16:42:55 -0600

    v.0000002

--- src/random-walks.R
+++ src/random-walks.R
@@ -2,20 +2,17 @@
 ## Random Walks
 ## October 2019
 
-GetRandomWalk <- function() {
-  # Add a standard normal at each step
-  initial.value + c(0, cumsum(rnorm(T)))
+gen_random_walk <- function() {
+  c(0, cumsum(rnorm(T)))
 }
 
 T <- 500
 k <- 250
 
-initial.value <- 0
-
 png(filename = "fig/one.png", width = 1.5*480, height = 1.5*480)
-set.seed(10923)
+set.seed(3937123)
 plot(-T:T, -T:T, axes = FALSE, ann = FALSE, type = "n")
-values <- replicate(k, GetRandomWalk())
+values <- replicate(k, gen_random_walk())
 gC <- round(runif(T, 1, 162))
 for (i in 1:k) {
   m <- matrix(c(0:T, values[, i]), nc = 2)
@@ -25,6 +22,6 @@ for (i in 1:k) {
   tmp <- t(G %*% t(m))
   cutoff <- sample(1:T, 1)
   lines(tmp[1:cutoff, 1], tmp[1:cutoff, 2], lwd = 0.75, 
-        col = rgb(10, gC[i], 10, alpha = runif(1, 50, 250), max = 255))
+        col = rgb(10, gC[i], 10, alpha = runif(1, 50, 150), max = 255))
 }
 dev.off()
